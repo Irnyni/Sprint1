26b144e85d9c9ce9af740fb07f3fb7db20aecfa5
felipe malengo <you@example.com>
2023-11-15 12:09:26 -0300

    mongoadd

--- app-backend/src/api/routes/posts.js
+++ app-backend/src/api/routes/posts.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const postsController = require('../controllers/postsController');
-
+const mongoose = require('mongoose');
 router.post('/', postsController.createPost);
 router.get('/', postsController.getAllPosts);
 
-module.exports = router;
+const postSchema = new mongoose.Schema({
+    descricao: String,
+    postagem: String,
+    imagem: String,
+  });
+const Post = mongoose.model('Post', postSchema);
+module.exports = router,Post;
--- server.js
+++ server.js
@@ -1,20 +1,28 @@
-const postsRouter = require('./app-backend/src/api/routes/posts');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors'); // Importe o módulo cors
+const cors = require('cors');
+const mongoose = require('mongoose');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Conectar ao MongoDB
+ mongoose.connect('mongodb+srv://irnyni:123@banco1.wurjpjq.mongodb.net/b1');
+
+
+
+// Verificar a conexão
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Erro de conexão ao MongoDB:'));
+db.once('open', function () {
+  console.log('Conectado ao MongoDB com sucesso');
+});
+
 app.use(bodyParser.json());
 app.use(cors());
 
-
-
-
-
-
-app.use(bodyParser.json());
-app.use(cors());
-
+const postsRouter = require('./app-backend/src/api/routes/posts');
 app.use('/posts', postsRouter);
 
 app.listen(PORT, () => {
@@ -22,3 +30,43 @@ app.listen(PORT, () => {
 });
 
 module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const postsRouter = require('./app-backend/src/api/routes/posts');
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors'); // Importe o módulo cors
+// const app = express();
+// const PORT = process.env.PORT || 5000;
+// app.use(bodyParser.json());
+// app.use(cors());
+
+
+
+
+
+
+// app.use(bodyParser.json());
+// app.use(cors());
+
+// app.use('/posts', postsRouter);
+
+// app.listen(PORT, () => {
+//   console.log(`Servidor Express está rodando na porta ${PORT}`);
+// });
+
+// module.exports = app;
